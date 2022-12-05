3e5c4328604b1d9be09ca8cc5c9fdafb9a59973d
paul ramain <paulramain44@gmail.com>
2022-12-05 09:10:16 +0100

    oui

--- mainProject/views/home.php
+++ mainProject/views/home.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="../public/assets/img/logoVeibae.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../public/assets/img/Reiraku_logo.png" type="image/x-icon">
     <link rel="stylesheet" href="../public/assets/css/home.css">
     <link rel="stylesheet" href="../public/assets/css/footer.css">
     <link rel="preconnect" href="https://fonts.googleapis.com"> 
